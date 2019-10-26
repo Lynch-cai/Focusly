9e2596ca80a591fa2e4e56e669afd632c0736805
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2019-10-26 22:33:34 +0200

    Add rain animation & volume slider

--- scripts/index.js
+++ scripts/index.js
@@ -11,12 +11,48 @@ let campfire_flames = document.querySelector('.index_campfire_fire')
 let leaves_falling = document.querySelectorAll('.index_tree_leaf_falling')
 let rain = document.querySelector('.index_rain')
 let icons = document.querySelectorAll('ul.index_icon_container a')
-let night_active = false, river_active = false, forest_active = false, campfire_active = false, rain_active = false
+let night_active = false, river_active = false, forest_active = false, campfire_active = false, rain_active = false, rain_first_click = true
 const sound_night = new Audio ('music/night.mp3')
 const sound_river = new Audio ('music/river.mp3')
 const sound_forest = new Audio ('music/forest.mp3')
 const sound_campfire = new Audio ('music/campfire.mp3')
 const sound_rain = new Audio ('music/rain.mp3')
+let music_slider = document.querySelectorAll('.index_icon_container li input')
+sound_night.volume = 0.5;
+sound_river.volume = 0.5;
+sound_forest.volume = 0.5;
+sound_campfire.volume = 0.5;
+sound_rain.volume = 0.5;
+music_slider[0].addEventListener( // change music value
+    'mousemove',
+    function(){
+        sound_night.volume = (music_slider[0].value / 100)-0.01
+    }
+)
+music_slider[1].addEventListener(
+    'mousemove',
+    function(){
+        sound_river.volume = (music_slider[1].value / 100)-0.01
+    }
+)
+music_slider[2].addEventListener(
+    'mousemove',
+    function(){
+        sound_forest.volume = (music_slider[2].value / 100)-0.01
+    }
+)
+music_slider[3].addEventListener(
+    'mousemove',
+    function(){
+        sound_campfire.volume = (music_slider[3].value / 100)-0.01
+    }
+)
+music_slider[4].addEventListener(
+    'mousemove',
+    function(){
+        sound_rain.volume = (music_slider[4].value / 100)-0.01
+    }
+)
 for (let i = 0; i < icons.length; i++) { //disable hover animation on pressed button
     icons[i].addEventListener(
         'click',
@@ -29,6 +65,7 @@ for (let i = 0; i < icons.length; i++) { //disable hover animation on pressed bu
 icons[0].addEventListener( //switch day to night / night to day & add shooting stars
     'click',
     ()=>{
+        music_slider[0].classList.toggle('fade-in')
         body.classList.toggle('time_switch')
         sun.classList.toggle('time_switch')
         moon.classList.toggle('time_switch')
@@ -49,6 +86,7 @@ icons[0].addEventListener( //switch day to night / night to day & add shooting s
 icons[1].addEventListener( //add river | have a glitch when we click to fast on the button
     'click',
     ()=>{
+        music_slider[1].classList.toggle('fade-in')
         alreadyPressed = false
         grass_platform.classList.toggle('part_platform')
         grass_platform.classList.toggle('whole_platform')
@@ -88,6 +126,7 @@ icons[1].addEventListener( //add river | have a glitch when we click to fast on 
 icons[2].addEventListener( //add tree
     'click',
     ()=>{
+        music_slider[2].classList.toggle('fade-in')
         for (let i = 0; i < 2; i++) {
             tree01[i].classList.toggle('spawn_tree')
         }
@@ -114,6 +153,7 @@ icons[2].addEventListener( //add tree
 icons[3].addEventListener( //add campfire
     'click',
     ()=>{
+        music_slider[3].classList.toggle('fade-in')
         for (let i = 0; i < campfire.length; i++) {
             campfire[i].classList.toggle('spawn_campfire')
         }
@@ -129,36 +169,42 @@ icons[3].addEventListener( //add campfire
         }
     }
 )
-icons[4].addEventListener( //add falling leaves
+icons[4].addEventListener( //add rain
     'click',
     ()=>{
+        music_slider[4].classList.toggle('fade-in')
         if (rain_active == false){ // Start music
             rain_active = true
             sound_rain.loop = true;
             sound_rain.play();
-            sound_rain.volume = 0.8;
         }
         else{
             rain_active = false
             sound_rain.pause();
         }
+        spawn_rain()
     }
 )
 function spawn_rain(){ //add rain
-    let rain_power = 50 // number of rain ray
+    let rain_power = 100 // number of rain ray
+    let temp_delay = 8/rain_power
     let rain_left_position = ''
     let rain_bottom_position = ''
-    for (let i = 0; i < rain_power; i++) {
-        let rain_ray = document.createElement('div')
-        rain.appendChild(rain_ray)
-        temp_left_pos = Math.floor(Math.random()*150)
-        
-
-        console.log(temp)
-
-        rain_ray.style
+    if(rain_first_click == true){
+        rain_first_click = false
+        for (let i = 0; i < rain_power; i++) {
+            let rain_ray = document.createElement('div')
+            rain.appendChild(rain_ray)
+            temp_left_pos = (Math.floor(Math.random()*620)+200)+'px' // rain ray random position
+            rain_ray.style.left = temp_left_pos
+            temp_delay = temp_delay+(8/rain_power)
+            rain_ray.style.animationDelay = temp_delay+'s'
+        }
     }
-
+    if(rain_active == true){
+        rain.style.display ='block'
+    }
+    else{
+        rain.style.display ='none'
+    }
 }
-
-spawn_rain()
